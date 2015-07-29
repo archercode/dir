a12637c40c52f00b98701b7ce76392b217d11ba5
archercode <tonnyquintos@gmail.com>
2015-07-29 20:43:37 +0800

    first commit

---
+++ src/com/example/e_directory/TnCActivity.java
@@ -0,0 +1,12 @@
+package com.example.e_directory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TnCActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+}
