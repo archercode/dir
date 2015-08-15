b9d3516e2a646eb830cf72e9037a6649f79a2d4a
archercode <tonnyquintos@gmail.com>
2015-08-15 16:31:54 +0800

    commit

--- gen/com/example/e_directory/R.java
+++ gen/com/example/e_directory/R.java
@@ -25,16 +25,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080007;
         public static final int btnGetList=0x7f080003;
         public static final int btnLocation=0x7f080002;
         public static final int editTextLocation=0x7f080000;
         public static final int layButtonH=0x7f080001;
         public static final int layloadingH=0x7f080004;
+        public static final int lv_cities=0x7f080006;
         public static final int progressBar1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int dialog_listview=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
